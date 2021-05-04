531742354de8c566bd7b81d7884e7e662dd5e6f6
foxman <lin.vladimir@gmail.com>
2021-05-05 03:00:10 +0800

    Commit Python Toolkits for AI

--- Networking/__init__.py
+++ Networking/__init__.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from . Download import Download as Download
-from . TLD      import TLD      as TLD
-from . TLDs     import TLDs     as TLDs
+from . Download   import Download     as Download
+from . HttpRPC    import HttpRPC      as HttpRPC
+from . SkypeRobot import SkypeRobot   as SkypeRobot
+from . SkypeRobot import SkypeWatcher as SkypeWatcher
+from . TLD        import TLD          as TLD
+from . TLDs       import TLDs         as TLDs
 
-__all__ = [ "Download" ,
-            "TLD"      ,
-            "TLDs"     ]
+__all__ = [ "Download"     ,
+            "HttpRPC"      ,
+            "SkypeRobot"   ,
+            "SkypeWatcher" ,
+            "TLD"          ,
+            "TLDs"         ]
