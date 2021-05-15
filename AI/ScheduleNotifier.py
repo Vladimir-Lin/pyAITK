33b0879ed110b7a00ab1986af0916f36710ccffb
foxman <lin.vladimir@gmail.com>
2021-05-15 20:58:48 +0800

    Commit AITK for Python Library

--- AI/ScheduleNotifier.py
+++ AI/ScheduleNotifier.py
@@ -1341,8 +1341,6 @@ class ScheduleNotifier (                                                   ) :
   def SyncPeriodsToGoogle         ( self , JSON                            ) :
     ##########################################################################
     PERIODs  = JSON               [ "Periods"                                ]
-    print ( json.dumps(JSON) )
-    print ( json.dumps(PERIODs) )
     if                            ( len ( PERIODs ) <= 0                   ) :
       return
     ##########################################################################
