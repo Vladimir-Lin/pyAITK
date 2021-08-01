9dc646d90a2955fa1104074cc6673a48b88b334b
foxman <lin.vladimir@gmail.com>
2021-08-01 21:10:12 +0800

    Commit AITK for Python Library

---
+++ TBL/TaiwanBLs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+## 台灣大樂透歷史紀錄
+##############################################################################
+import os
+import sys
+import getopt
+import time
+import datetime
+import logging
+import requests
+import threading
+import gettext
+import json
+import codecs
+import random
+import numpy
+import scipy
+from   scipy . optimize import curve_fit as CurveFit
+from . TaiwanBLs        import TaiwanBLs as TaiwanBLs
+##############################################################################
+class TaiwanBLs        (                                                   ) :
+  ############################################################################
+  def __init__         ( self                                              ) :
+    ##########################################################################
+    ##########################################################################
+    return
+  ############################################################################
+  def __del__          ( self                                              ) :
+    return
+##############################################################################
