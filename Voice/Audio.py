ee6d13c3bdaa69c0233d48e6e960d48fe86ea964
Vladimir-Lin <lin.vladimir@gmail.com>
2021-09-27 05:51:30 +0800

    a

--- Voice/Audio.py
+++ Voice/Audio.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#############################################################################
+#############################################################################
 import os
 import os . path
 import sys
@@ -8,27 +10,28 @@ import getopt
 import logging
 import threading
 from   playsound import playsound
-
+#############################################################################
 class AudioPlayer ( ) :
-
+  ###########################################################################
   def __init__ ( self , mappings = { } ) :
     self . Initialize ( mappings )
-
+  ###########################################################################
   def Initialize ( self , mappings ) :
     self . Mappings = mappings
     return True
-
+  ###########################################################################
   def PlayFile ( self , Filename ) :
     if ( not os . path . isfile ( Filename ) ) :
       return False
     playsound  ( Filename )
     return True
-
+  ###########################################################################
   def Play ( self , Filename ) :
     threading . Thread ( target = self . PlayFile , args = ( Filename , ) ) . start ( )
     return
-
+  ###########################################################################
   def Notice ( self , key ) :
     if ( key in self . Mappings ) :
       self . Play ( self . Mappings [ key ] )
     return
+#############################################################################
