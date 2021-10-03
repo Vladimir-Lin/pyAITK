3073ade7d1aeeed23f399942042ec0766019387a
Vladimir-Lin <lin.vladimir@gmail.com>
2021-10-03 11:44:38 +0800

    a

--- Qt/IconDock.py
+++ Qt/IconDock.py
@@ -49,128 +49,14 @@ class IconDock           ( ListDock                                        ) :
     super ( ) . __init__ ( parent , plan                                     )
     ##########################################################################
     return
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
 ##############################################################################
-
-"""
-
-class Q_COMPONENTS_EXPORT ListDock : public ListWidget
-                                   , public AttachDock
-{
-  Q_OBJECT
-  public:
-
-    int                 dockingOrientation ;
-    Qt::DockWidgetArea  dockingPlace       ;
-    Qt::DockWidgetAreas dockingPlaces      ;
-
-    explicit ListDock        (StandardConstructor) ;
-    virtual ~ListDock        (void);
-
-  protected:
-
-  private:
-
-  public slots:
-
-    virtual void Docking     (QMainWindow *       Main    ,
-                              QString             title   ,
-                              Qt::DockWidgetArea  area    ,
-                              Qt::DockWidgetAreas areas ) ;
-    virtual void DockIn      (bool shown);
-
-  protected slots:
-
-    virtual void DockingMenu (MenuManager & Menu) ;
-    virtual bool RunDocking  (MenuManager & Menu,QAction * action) ;
-    void Visible             (bool visible);
-
-  private slots:
-
-  signals:
-
-    DockSignals ;
-
-};
-
-
-
-
-
-#include <qtcomponents.h>
-
-N::ListDock:: ListDock           ( QWidget * parent , Plan * p )
-            : ListWidget         (           parent ,        p )
-            , AttachDock         (                           p )
-            , dockingOrientation ( 0                           )
-            , dockingPlace       ( Qt::RightDockWidgetArea     )
-            , dockingPlaces      ( Qt::TopDockWidgetArea       |
-                                   Qt::BottomDockWidgetArea    |
-                                   Qt::LeftDockWidgetArea      |
-                                   Qt::RightDockWidgetArea     )
-{
-  WidgetClass                                            ;
-  setFunction ( N::AttachDock::FunctionDocking , true )  ;
-  LocalMsgs [ AttachToMdi  ] = tr("Move to window area") ;
-  LocalMsgs [ AttachToDock ] = tr("Move to dock area"  ) ;
-}
-
-N::ListDock::~ListDock (void)
-{
-}
-
-void N::ListDock::Docking(QMainWindow       * Main  ,
-                          QString             title ,
-                          Qt::DockWidgetArea  area  ,
-                          Qt::DockWidgetAreas areas )
-{
-  AttachDock::Docking(Main,this,title,area,areas) ;
-  nConnect(Dock,SIGNAL(visibilityChanged(bool))   ,
-           this,SLOT  (Visible          (bool)) ) ;
-}
-
-void N::ListDock::Visible(bool visible)
-{
-  Visiblity(visible) ;
-}
-
-void N::ListDock::DockIn(bool shown)
-{
-  Show(shown);
-}
-
-void N::ListDock::DockingMenu(MenuManager & Menu)
-{
-  if ( ! isFunction ( N::AttachDock::FunctionDocking ) ) return          ;
-  QMdiSubWindow  * mdi    = Casting(QMdiSubWindow,parent())              ;
-  QDockWidget    * dock   = Casting(QDockWidget  ,parent())              ;
-  if (NotNull(dock) || NotNull(mdi)) Menu . addSeparator ( )             ;
-  nIfSafe(dock) Menu . add ( AttachToMdi  , LocalMsgs [ AttachToMdi  ] ) ;
-  nIfSafe(mdi ) Menu . add ( AttachToDock , LocalMsgs [ AttachToDock ] ) ;
-}
-
-bool N::ListDock::RunDocking(MenuManager & Menu,QAction * action)
-{
-  switch (Menu[action])                        {
-    case AttachToMdi                           :
-      emit attachMdi (this,dockingOrientation) ;
-    break                                      ;
-    case AttachToDock                          :
-      emit attachDock                          (
-        this                                   ,
-        windowTitle()                          ,
-        dockingPlace                           ,
-        dockingPlaces                        ) ;
-    break                                      ;
-    default: return false                      ;
-  }                                            ;
-  return true                                  ;
-}
-
-
-
-
-"""
-
-
-
-
