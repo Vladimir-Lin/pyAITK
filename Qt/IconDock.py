1d700fce23079d15be3737aef174b628b181e1da
foxman <lin.vladimir@gmail.com>
2021-10-03 11:51:55 +0800

    a

--- Qt/IconDock.py
+++ Qt/IconDock.py
@@ -60,3 +60,72 @@ class IconDock           ( ListDock                                        ) :
   ############################################################################
   ############################################################################
 ##############################################################################
+
+
+"""
+
+class Q_COMPONENTS_EXPORT IconDock : public ListDock
+{
+  Q_OBJECT
+  public:
+
+    explicit IconDock      (StandardConstructor) ;
+    virtual ~IconDock      (void);
+
+  protected:
+
+    virtual void Configure (void);
+
+  private:
+
+  public slots:
+
+    virtual bool startup   (void);
+
+  protected slots:
+
+    virtual bool Menu      (QPoint pos) ;
+
+  private slots:
+
+  signals:
+
+};
+
+N::IconDock:: IconDock (QWidget * parent,Plan * p)
+            : ListDock (          parent,       p)
+{
+  WidgetClass   ;
+  Configure ( ) ;
+}
+
+N::IconDock::~IconDock (void)
+{
+}
+
+void N::IconDock::Configure(void)
+{
+  setViewMode                  (IconMode             ) ;
+  setIconSize                  (QSize(128,128)       ) ;
+  setGridSize                  (QSize(140,192)       ) ;
+  setDragDropMode              (DropOnly             ) ;
+  setResizeMode                (QListView::Adjust    ) ;
+  setWordWrap                  (true                 ) ;
+  setHorizontalScrollBarPolicy (Qt::ScrollBarAsNeeded) ;
+  setVerticalScrollBarPolicy   (Qt::ScrollBarAsNeeded) ;
+  setMinimumSize               (QSize(144,200)       ) ;
+}
+
+bool N::IconDock::startup(void)
+{
+  if (IsNull(plan)) return false ;
+  return true                    ;
+}
+
+bool N::IconDock::Menu(QPoint pos)
+{ Q_UNUSED ( pos ) ;
+  return false     ;
+}
+
+"""
+
