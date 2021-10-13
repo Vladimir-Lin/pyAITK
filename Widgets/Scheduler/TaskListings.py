d824a4c46991d0910f60f8d8af59cdf96691be8a
Vladimir-Lin <lin.vladimir@gmail.com>
2021-10-14 03:29:14 +0800

    a

---
+++ Widgets/Scheduler/TaskListings.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+## TaskListings
+##############################################################################
+import os
+import sys
+import getopt
+import time
+import requests
+import threading
+import gettext
+import json
+##############################################################################
+from   PyQt5                          import QtCore
+from   PyQt5                          import QtGui
+from   PyQt5                          import QtWidgets
+##############################################################################
+from   PyQt5 . QtCore                 import QObject
+from   PyQt5 . QtCore                 import pyqtSignal
+from   PyQt5 . QtCore                 import pyqtSlot
+from   PyQt5 . QtCore                 import Qt
+from   PyQt5 . QtCore                 import QPoint
+from   PyQt5 . QtCore                 import QPointF
+##############################################################################
+from   PyQt5 . QtGui                  import QIcon
+from   PyQt5 . QtGui                  import QCursor
+from   PyQt5 . QtGui                  import QKeySequence
+##############################################################################
+from   PyQt5 . QtWidgets              import QApplication
+from   PyQt5 . QtWidgets              import QWidget
+from   PyQt5 . QtWidgets              import qApp
+from   PyQt5 . QtWidgets              import QMenu
+from   PyQt5 . QtWidgets              import QAction
+from   PyQt5 . QtWidgets              import QShortcut
+from   PyQt5 . QtWidgets              import QMenu
+from   PyQt5 . QtWidgets              import QTreeWidget
+from   PyQt5 . QtWidgets              import QTreeWidgetItem
+from   PyQt5 . QtWidgets              import QLineEdit
+from   PyQt5 . QtWidgets              import QComboBox
+from   PyQt5 . QtWidgets              import QSpinBox
+##############################################################################
+from   AITK  . Qt . MenuManager       import MenuManager   as MenuManager
+from   AITK  . Qt . MajorListings     import MajorListings as MajorListings
+##############################################################################
+from   AITK . Calendars . StarDate    import StarDate
+from   AITK . Calendars . Periode     import Periode
+##############################################################################
+class TaskListings       ( MajorListings                                   ) :
+  ############################################################################
+  def __init__           ( self , parent = None , plan = None              ) :
+    ##########################################################################
+    super ( ) . __init__ (        parent        , plan                       )
+    ##########################################################################
+    ##########################################################################
+    return
+  ############################################################################
+  def ObtainUuidsQuery                ( self                               ) :
+    ##########################################################################
+    TABLE = self . Tables [ "Tasks" ]
+    QQ    = f"select `uuid` from {TABLE} where ( `used` = 1 ) order by `id` desc ;"
+    ##########################################################################
+    return QQ
+  ############################################################################
+  def Prepare                 ( self                                       ) :
+    ##########################################################################
+    LABELs = [ "任務名稱" ]
+    self   . setCentralLabels ( LABELs                                       )
+    ##########################################################################
+    self   . setPrepared      ( True                                         )
+    ##########################################################################
+    return
+  ############################################################################
+  def AssureUuidItem               ( self , item , uuid , name             ) :
+    ##########################################################################
+    """
+    DB      = self . ConnectDB     (                                         )
+    if                             ( DB == None                            ) :
+      return
+    ##########################################################################
+    TSKTAB  = self . Tables        [ "Tasks"                                 ]
+    PRDTAB  = self . Tables        [ "Periods"                               ]
+    NAMTAB  = self . Tables        [ "Names"                                 ]
+    HEAD    = 5702000000000000000
+    ##########################################################################
+    DB      . LockWrites           ( [ PRJTAB , PRDTAB , NAMTAB ]            )
+    ##########################################################################
+    if                             ( uuid <= 0                             ) :
+      ########################################################################
+      uuid  = DB . LastUuid        ( PRJTAB , "uuid" , HEAD                  )
+      DB    . AddUuid              ( PRJTAB , uuid   , 1                     )
+      ########################################################################
+      NOW   = StarDate             (                                         )
+      NOW   . Now                  (                                         )
+      CDT   = NOW . Stardate
+      ########################################################################
+      PRD   = Periode              (                                         )
+      PRID  = PRD  . GetUuid       ( DB , PRDTAB                             )
+      ########################################################################
+      PRD   . Realm    = uuid
+      PRD   . Role     = 71
+      PRD   . Item     = 1
+      PRD   . States   = 0
+      PRD   . Creation = CDT
+      PRD   . Modified = CDT
+      Items =                      [ "realm"                               , \
+                                     "role"                                , \
+                                     "item"                                , \
+                                     "states"                              , \
+                                     "creation"                            , \
+                                     "modified"                              ]
+      PRD   . UpdateItems          ( DB , PRDTAB , Items                     )
+    ##########################################################################
+    self    . AssureUuidName       ( DB , NAMTAB , uuid , name               )
+    ##########################################################################
+    DB      . Close                (                                         )
+    ##########################################################################
+    item    . setData              ( 0 , Qt . UserRole , uuid                )
+    """
+    ##########################################################################
+    return
+  ############################################################################
+  def Menu                         ( self , pos                            ) :
+    ##########################################################################
+    items  = self . selectedItems  (                                         )
+    mm     = MenuManager           ( self                                    )
+    ##########################################################################
+    TRX    = self . Translations
+    ##########################################################################
+    mm     . addAction             ( 1001 ,  TRX [ "UI::Refresh"  ]          )
+    mm     . addAction             ( 1101 ,  TRX [ "UI::Insert"   ]          )
+    mm     . addSeparator          (                                         )
+    if                             ( len ( items ) == 1                    ) :
+      if                           ( self . EditAllNames != None           ) :
+        mm . addAction             ( 1601 ,  TRX [ "UI::EditNames" ]         )
+        mm . addSeparator          (                                         )
+    ##########################################################################
+    mm     = self . LocalityMenu   ( mm                                      )
+    ##########################################################################
+    mm     . setFont               ( self    . font ( )                      )
+    aa     = mm . exec_            ( QCursor . pos  ( )                      )
+    at     = mm . at               ( aa                                      )
+    ##########################################################################
+    if                             ( self . HandleLocalityMenu ( at )      ) :
+      return True
+    ##########################################################################
+    if                             ( at == 1001                            ) :
+      self . startup               (                                         )
+      return True
+    ##########################################################################
+    if                             ( at == 1101                            ) :
+      self . InsertItem            (                                         )
+      return True
+    ##########################################################################
+    if                             ( at == 1601                            ) :
+      uuid = self . itemUuid       ( items [ 0 ] , 0                         )
+      NAM  = self . Tables         [ "Names"                                 ]
+      self . EditAllNames          ( self , "Projects" , uuid , NAM          )
+      return True
+    ##########################################################################
+    return True
+##############################################################################
--- Widgets/Scheduler/__init__.py
+++ Widgets/Scheduler/__init__.py
@@ -2,7 +2,9 @@
 ##############################################################################
 from . ProjectListings import ProjectListings as ProjectListings
 from . ProjectsView    import ProjectsView    as ProjectsView
+from . TaskListings    import TaskListings    as TaskListings
 ##############################################################################
 __all__ = [ "ProjectListings"                                              , \
-            "ProjectsView"                                                   ]
+            "ProjectsView"                                                 , \
+            "TaskListings"                                                   ]
 ##############################################################################
