fd996c7fb678f7db98d02c283e03e278e52c336f
Vladimir-Lin <lin.vladimir@gmail.com>
2021-10-17 02:04:13 +0800

    a

--- Widgets/__init__.py
+++ Widgets/__init__.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
-
+##############################################################################
+from . import Biology
+from . import Commons
 from . import Networking
-
-__all__ = [ "Networking" ]
+from . import Pictures
+from . import Scheduler
+from . import Society
+from . import Videos
+##############################################################################
+__all__ = [ "Biology"                                                      , \
+            "Commons"                                                      , \
+            "Networking"                                                   , \
+            "Pictures"                                                     , \
+            "Scheduler"                                                    , \
+            "Society"                                                      , \
+            "Videos"                                                         ]
+##############################################################################
