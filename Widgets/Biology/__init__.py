ceacd249105ac6e2dc6247cf44deb213457487b5
Vladimir-Lin <lin.vladimir@gmail.com>
2021-11-01 23:05:38 +0800

    a

--- Widgets/Biology/__init__.py
+++ Widgets/Biology/__init__.py
@@ -6,11 +6,13 @@ from . EyeShapeListings   import EyeShapeListings   as EyeShapeListings
 from . HairColorListings  import HairColorListings  as HairColorListings
 from . FaceShapeListings  import FaceShapeListings  as FaceShapeListings
 from . HumanBloodListings import HumanBloodListings as HumanBloodListings
+from . BloodGroupListings import BloodGroupListings as BloodGroupListings
 ##############################################################################
 __all__ = [ "SexualityListings"                                            , \
             "EyeColorListings"                                             , \
             "EyeShapeListings"                                             , \
             "HairColorListings"                                            , \
             "FaceShapeListings"                                            , \
-            "HumanBloodListings"                                             ]
+            "HumanBloodListings"                                           , \
+            "BloodGroupListings"                                             ]
 ##############################################################################
