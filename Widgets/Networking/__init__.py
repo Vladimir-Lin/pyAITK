6308f55b5986b740fde95b7315bed48d21e892f7
foxman <lin.vladimir@gmail.com>
2021-11-04 10:31:57 +0800

    a

--- Widgets/Networking/__init__.py
+++ Widgets/Networking/__init__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
+from . TldsWidget      import TldsWidget      as TldsWidget
+from . SldsWidget      import SldsWidget      as SldsWidget
+from . UrlsWidget      import UrlsWidget      as UrlsWidget
+from . DomainsWidget   import DomainsWidget   as DomainsWidget
 from . WebPageListings import WebPageListings as WebPageListings
 ##############################################################################
-__all__ = [ "WebPageListings"                                                ]
+__all__ = [ "TldsWidget"                                                   , \
+            "SldsWidget"                                                   , \
+            "UrlsWidget"                                                   , \
+            "DomainsWidget"                                                , \
+            "WebPageListings"                                                ]
 ##############################################################################
