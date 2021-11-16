a287ae0796aa7c1afeb5088b510a4569f5f9cfc3
Vladimir-Lin <lin.vladimir@gmail.com>
2021-11-16 09:53:06 +0800

    Commit AITK for Python Library

---
+++ Widgets/Networking/ItuWidget.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+## WebPageListings
+##############################################################################
+import os
+import sys
+import getopt
+import time
+import requests
+import threading
+import gettext
+import json
+##############################################################################
+from   PyQt5                          import QtCore
+from   PyQt5                          import QtGui
+from   PyQt5                          import QtWidgets
+##############################################################################
+from   PyQt5 . QtCore                 import QObject
+from   PyQt5 . QtCore                 import pyqtSignal
+from   PyQt5 . QtCore                 import pyqtSlot
+from   PyQt5 . QtCore                 import Qt
+from   PyQt5 . QtCore                 import QPoint
+from   PyQt5 . QtCore                 import QPointF
+from   PyQt5 . QtCore                 import QSize
+from   PyQt5 . QtCore                 import QSizeF
+from   PyQt5 . QtCore                 import QUrl
+##############################################################################
+from   PyQt5 . QtGui                  import QIcon
+from   PyQt5 . QtGui                  import QCursor
+from   PyQt5 . QtGui                  import QKeySequence
+from   PyQt5 . QtGui                  import QDesktopServices
+##############################################################################
+from   PyQt5 . QtWidgets              import QApplication
+from   PyQt5 . QtWidgets              import QWidget
+from   PyQt5 . QtWidgets              import qApp
+from   PyQt5 . QtWidgets              import QAction
+from   PyQt5 . QtWidgets              import QShortcut
+from   PyQt5 . QtWidgets              import QMenu
+from   PyQt5 . QtWidgets              import QAbstractItemView
+from   PyQt5 . QtWidgets              import QTreeWidget
+from   PyQt5 . QtWidgets              import QTreeWidgetItem
+from   PyQt5 . QtWidgets              import QLineEdit
+from   PyQt5 . QtWidgets              import QComboBox
+from   PyQt5 . QtWidgets              import QSpinBox
+##############################################################################
+from   AITK  . Qt . MenuManager       import MenuManager as MenuManager
+from   AITK  . Qt . TreeDock          import TreeDock    as TreeDock
+from   AITK  . Qt . LineEdit          import LineEdit    as LineEdit
+from   AITK  . Qt . ComboBox          import ComboBox    as ComboBox
+from   AITK  . Qt . SpinBox           import SpinBox     as SpinBox
+##############################################################################
+from   AITK  . Essentials . Relation  import Relation
+##############################################################################
+from   AITK . Calendars . StarDate    import StarDate
+from   AITK . Calendars . Periode     import Periode
+##############################################################################
+class TldsWidget                ( TreeDock                                 ) :
+  ############################################################################
+  def __init__                  ( self , parent = None                     ) :
+    ##########################################################################
+    ##########################################################################
+    return
+  ############################################################################
+  def startup                   ( self                                     ) :
+    return
+##############################################################################
