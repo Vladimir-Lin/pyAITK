9611a9c57f9900dcbf0326b9a9f5cec1c9dd92f0
Vladimir-Lin <lin.vladimir@gmail.com>
2021-11-18 18:58:33 +0800

    Commit AITK for Python Library

--- People/__init__.py
+++ People/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-from . Gallery  import Gallery  as Gallery
-from . Picture  import Picture  as Picture
-from . Face     import Face     as Face
+from . People import People as People
 ##############################################################################
-__all__ = [ "Picture" , "Gallery" , "Face"                                   ]
+__all__ = [ "People"                                                         ]
 ##############################################################################
