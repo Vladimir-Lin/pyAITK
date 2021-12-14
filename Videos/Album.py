817b17dc39b876f7e5a8f2d45d02c0c8b330ec85
foxman <lin.vladimir@gmail.com>
2021-12-14 17:36:42 +0800

    Commit AITK for Python Library

---
+++ Videos/Album.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+## 圖庫
+##############################################################################
+import os
+import sys
+import time
+import datetime
+import logging
+import requests
+import threading
+import gettext
+import binascii
+import hashlib
+import base64
+##############################################################################
+from   AITK  . Database   . Connection     import Connection     as Connection
+##############################################################################
+from   AITK  . Documents  . Name           import Name           as NameItem
+from   AITK  . Documents  . Name           import Naming         as Naming
+from   AITK  . Documents  . Notes          import Notes          as NoteItem
+from   AITK  . Documents  . Variables      import Variables      as VariableItem
+from   AITK  . Documents  . ParameterQuery import ParameterQuery as ParameterQuery
+##############################################################################
+from   AITK  . Calendars  . StarDate       import StarDate       as StarDate
+from   AITK  . Calendars  . Periode        import Periode        as Periode
+from   AITK  . Essentials . Relation       import Relation       as Relation
+##############################################################################
+class Album       (                                                        ) :
+  ############################################################################
+  def __init__    ( self                                                   ) :
+    ##########################################################################
+    ##########################################################################
+    return
+  ############################################################################
+  def __del__     ( self                                                   ) :
+    pass
+  ############################################################################
+  def ConnectToPictures  ( self , DB , TABLE , UUID , T1 , UUIDs           ) :
+    ##########################################################################
+    REL = Relation       (                                                   )
+    REL . set            ( "first" , UUID                                    )
+    REL . setT1          ( T1                                                )
+    REL . setT2          ( "Picture"                                         )
+    REL . setRelation    ( "Subordination"                                   )
+    REL . Joins          ( DB , TABLE , UUIDs                                )
+    ##########################################################################
+    return
+  ############################################################################
+  def ConnectToGalleries ( self , DB , TABLE , UUID , T1 , UUIDs           ) :
+    ##########################################################################
+    REL = Relation       (                                                   )
+    REL . set            ( "first" , UUID                                    )
+    REL . setT1          ( T1                                                )
+    REL . setT2          ( "Gallery"                                         )
+    REL . setRelation    ( "Subordination"                                   )
+    REL . Joins          ( DB , TABLE , UUIDs                                )
+    ##########################################################################
+    return
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+  ############################################################################
+##############################################################################
