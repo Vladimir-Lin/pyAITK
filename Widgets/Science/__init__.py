1b6742dda8c8c762ceef9c01b417336c5230f0bd
Vladimir-Lin <lin.vladimir@gmail.com>
2021-12-16 20:19:15 +0800

    Commit AITK for Python Library

--- Widgets/Science/__init__.py
+++ Widgets/Science/__init__.py
@@ -2,9 +2,11 @@
 ##############################################################################
 from . SpectrumColors        import SpectrumColors        as SpectrumColors
 from . StellarSpectrumWidget import StellarSpectrumWidget as StellarSpectrumWidget
+from . StellarObjectListings import StellarObjectListings as StellarObjectListings
 from . CelestialListings     import CelestialListings     as CelestialListings
 ##############################################################################
 __all__ = [ "CelestialListings"                                            , \
+            "StellarObjectListings"                                        , \
             "SpectrumColors"                                               , \
             "StellarSpectrumWidget"                                          ]
 ##############################################################################
