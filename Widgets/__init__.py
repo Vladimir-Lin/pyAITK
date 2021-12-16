b91b26ade517d87e78696d9d48cabef2c4fef966
Vladimir-Lin <lin.vladimir@gmail.com>
2021-12-16 20:26:22 +0800

    Commit AITK for Python Library

--- Widgets/__init__.py
+++ Widgets/__init__.py
@@ -2,19 +2,25 @@
 ##############################################################################
 from . import Biology
 from . import Commons
+from . import Finance
+from . import Languages
 from . import Networking
 from . import Pictures
 from . import Scheduler
 from . import Science
 from . import Society
+from . import TBL
 from . import Videos
 ##############################################################################
 __all__ = [ "Biology"                                                      , \
             "Commons"                                                      , \
+            "Finance"                                                      , \
+            "Languages"                                                    , \
             "Networking"                                                   , \
             "Pictures"                                                     , \
             "Scheduler"                                                    , \
             "Science"                                                      , \
             "Society"                                                      , \
+            "TBL"                                                          , \
             "Videos"                                                         ]
 ##############################################################################
