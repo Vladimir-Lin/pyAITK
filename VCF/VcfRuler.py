75633ad81fc532c29e945c4a168f4f23a620e898
foxman <lin.vladimir@gmail.com>
2022-03-12 11:09:03 +0800

    Commit AITK for Python Library

--- VCF/VcfRuler.py
+++ VCF/VcfRuler.py
@@ -28,6 +28,7 @@ from   PyQt5 . QtGui                  import QFontMetricsF
 from   PyQt5 . QtGui                  import QPen
 from   PyQt5 . QtGui                  import QBrush
 from   PyQt5 . QtGui                  import QKeySequence
+from   PyQt5 . QtGui                  import QPainterPath
 ##############################################################################
 from   PyQt5 . QtWidgets              import QApplication
 from   PyQt5 . QtWidgets              import qApp
@@ -36,13 +37,20 @@ from   PyQt5 . QtWidgets              import QGraphicsView
 ##############################################################################
 from         . VcfRectangle           import VcfRectangle as VcfRectangle
 ##############################################################################
-class VcfRuler        ( VcfRectangle                                       ) :
+class VcfRuler                  ( VcfRectangle                             ) :
   ############################################################################
-  def __init__        ( self                                               , \
-                        parent = None                                      , \
-                        item   = None                                      , \
-                        plan   = None                                      ) :
+  vrEast  = 0
+  vrSouth = 1
+  vrWest  = 2
+  vrNorth = 3
+  ############################################################################
+  def __init__                  ( self                                     , \
+                                  parent = None                            , \
+                                  item   = None                            , \
+                                  plan   = None                            ) :
     ##########################################################################
+    super ( ) . __init__        ( parent , item , plan                       )
+    self . setVcfRulerDefaults  (                                            )
     ##########################################################################
     return
   ############################################################################
@@ -50,194 +58,175 @@ class VcfRuler        ( VcfRectangle                                       ) :
     ##########################################################################
     ##########################################################################
     return
+  ############################################################################
+  def setVcfRulerDefaults       ( self                                     ) :
+    ##########################################################################
+    self . Direction = self . vrNorth
+    ##########################################################################
+    self . Painter   . addMap   ( "Default" , 0                              )
+    self . Painter   . addPen   ( 0 , QColor ( 192 , 192 , 192 )             )
+    self . Painter   . addBrush ( 0 , QColor ( 252 , 252 , 252 )             )
+    ##########################################################################
+    return
+  ############################################################################
+  def paint         ( self , painter , options , widget                    ) :
+    ##########################################################################
+    self . Painting (        painter , self . ScreenRect , True , True       )
+    ##########################################################################
+    return
+  ############################################################################
+  def Painting                ( self , p , region , clip , color           ) :
+    ##########################################################################
+    self . pushPainters       ( p                                            )
+    ##########################################################################
+    self . Painter . drawRect ( p , "Default" , self . ScreenRect            )
+    ##########################################################################
+    if                        ( self . Painter . pathes . contains ( 0 )   ) :
+      p  . drawPath           ( self . Painter . pathes [ 0 ]                )
+    ##########################################################################
+    self . popPainters        ( p                                            )
+    ##########################################################################
+    return
+  ############################################################################
+  def CreatePath        ( self                                             ) :
+    ##########################################################################
+    self . Painter . pathes [ 0 ] = QPainterPath (                           )
+    ##########################################################################
+    if                  ( self . Direction == self . vrEast                ) :
+      self . PaintEast  ( self . Painter . pathes [ 0 ]                      )
+      return
+    ##########################################################################
+    if                  ( self . Direction == self . vrSouth               ) :
+      self . PaintSouth ( self . Painter . pathes [ 0 ]                      )
+      return
+    ##########################################################################
+    if                  ( self . Direction == self . vrWest                ) :
+      self . PaintWest  ( self . Painter . pathes [ 0 ]                      )
+      return
+    ##########################################################################
+    if                  ( self . Direction == self . vrNorth               ) :
+      self . PaintNorth ( self . Painter . pathes [ 0 ]                      )
+      return
+    ##########################################################################
+    return
+  ############################################################################
+  def PaintEast  ( self , p ) :
+    ##########################################################################
+    """
+    QRectF  dr = ScreenRect  ;
+    qreal   ep = dr.height() ;
+    qreal   dw = dr.width () ;
+    qreal   mw = dw / 2      ;
+    qreal   mm = dw / 4      ;
+    qreal   dl = 0           ;
+    int     di = 0           ;
+    QPointF xy               ;
+    do {
+      dl  = di ;
+      dl /= 10 ;
+      xy.setX(dl);
+      xy.setY(dl);
+      xy  = toPaper(xy);
+      dl  = xy.x();
+      if (dl<ep) {
+        p->moveTo(dr.left(),dr.top()+dl);
+        if ((di%10)==0) p->lineTo(dr.right()   ,dr.top()+dl); else
+        if ((di% 5)==0) p->lineTo(dr.left ()+mw,dr.top()+dl); else
+                        p->lineTo(dr.left ()+mm,dr.top()+dl);
+      };
+      di ++ ;
+    } while (dl<ep);
+    """
+    ##########################################################################
+    return
+  ############################################################################
+  def PaintSouth ( self , p ) :
+    ##########################################################################
+    """
+    QRectF  dr = ScreenRect  ;
+    qreal   ep = dr.width () ;
+    qreal   dh = dr.height() ;
+    qreal   mh = dh / 2      ;
+    qreal   mm = dh / 4      ;
+    qreal   dl = 0           ;
+    int     di = 0           ;
+    QPointF xy               ;
+    do {
+      dl  = di ;
+      dl /= 10 ;
+      xy.setX(dl);
+      xy.setY(dl);
+      xy  = toPaper(xy);
+      dl  = xy.x();
+      if (dl<ep) {
+        p->moveTo(dr.left()+dl,dr.top());
+        if ((di%10)==0) p->lineTo(dr.left()+dl,dr.bottom()   ); else
+        if ((di% 5)==0) p->lineTo(dr.left()+dl,dr.top   ()+mh); else
+                        p->lineTo(dr.left()+dl,dr.top   ()+mm);
+      };
+      di ++ ;
+    } while (dl<ep);
+    """
+    ##########################################################################
+    return
+  ############################################################################
+  def PaintWest  ( self , p ) :
+    ##########################################################################
+    """
+    QRectF  dr = ScreenRect  ;
+    qreal   ep = dr.height() ;
+    qreal   dw = dr.width () ;
+    qreal   mw = dw / 2      ;
+    qreal   mm = dw / 4      ;
+    qreal   dl = 0           ;
+    int     di = 0           ;
+    QPointF xy               ;
+    do {
+      dl  = di ;
+      dl /= 10 ;
+      xy.setX(dl);
+      xy.setY(dl);
+      xy  = toPaper(xy);
+      dl  = xy.x();
+      if (dl<ep) {
+        p->moveTo(dr.right(),dr.top()+dl);
+        if ((di%10)==0) p->lineTo(dr.left ()   ,dr.top()+dl); else
+        if ((di% 5)==0) p->lineTo(dr.right()-mw,dr.top()+dl); else
+                        p->lineTo(dr.right()-mm,dr.top()+dl);
+      };
+      di ++ ;
+    } while (dl<ep);
+    """
+    ##########################################################################
+    return
+  ############################################################################
+  def PaintNorth ( self , p ) :
+    ##########################################################################
+    """
+    QRectF  dr = ScreenRect ;
+    qreal   ep = dr.width ();
+    qreal   dh = dr.height();
+    qreal   mh = dh / 2     ;
+    qreal   mm = dh / 4     ;
+    qreal   dl = 0          ;
+    int     di = 0          ;
+    QPointF xy              ;
+    do {
+      dl  = di ;
+      dl /= 10 ;
+      xy.setX(dl);
+      xy.setY(dl);
+      xy  = toPaper(xy);
+      dl  = xy.x();
+      if (dl<ep) {
+        p->moveTo(dr.left()+dl,dr.bottom());
+        if ((di%10)==0) p->lineTo(dr.left()+dl,dr.top   ()   ); else
+        if ((di% 5)==0) p->lineTo(dr.left()+dl,dr.bottom()-mh); else
+                        p->lineTo(dr.left()+dl,dr.bottom()-mm);
+      };
+      di ++    ;
+    } while (dl<ep);
+    """
+    ##########################################################################
+    return
 ##############################################################################
-"""
-class Q_COMPONENTS_EXPORT VcfRuler : public VcfRectangle
-{
-  Q_OBJECT
-  public:
-
-    Direction direction ;
-
-    enum { Type = UserType + VCF::Ruler };
-    virtual int type(void) const { return Type; }
-
-    explicit VcfRuler  (QObject * parent,QGraphicsItem * item,Plan * plan = NULL) ;
-    virtual ~VcfRuler  (void);
-
-    virtual void paint (QPainter * painter,const QStyleOptionGraphicsItem * option,QWidget * widget = 0);
-
-  protected:
-
-  private:
-
-  public slots:
-
-    void CreatePath    (void) ;
-    void Paint         (QPainter * painter,QRectF Region,bool clip,bool color);
-    void PaintNorth    (QPainterPath * painter);
-    void PaintSouth    (QPainterPath * painter);
-    void PaintEast     (QPainterPath * painter);
-    void PaintWest     (QPainterPath * painter);
-
-  protected slots:
-
-  private slots:
-
-  signals:
-
-};
-
-N::VcfRuler:: VcfRuler     (QObject * parent,QGraphicsItem * item,Plan * p)
-            : VcfRectangle (          parent,                item,       p)
-            , direction    (North                                         )
-{
-  Painter . addMap   ( "Default" , 0                  ) ;
-  Painter . addPen   ( 0 , QColor ( 192 , 192 , 192 ) ) ;
-  Painter . addBrush ( 0 , QColor ( 252 , 252 , 252 ) ) ;
-}
-
-N::VcfRuler::~VcfRuler(void)
-{
-}
-
-void N::VcfRuler::paint(QPainter * p,const QStyleOptionGraphicsItem *,QWidget *)
-{
-  Paint(p,ScreenRect,true,true);
-}
-
-void N::VcfRuler::Paint(QPainter * p,QRectF Region,bool clip,bool color)
-{
-  pushPainters ( p )         ;
-  Painter . drawRect ( p , "Default" , ScreenRect ) ;
-  if (Painter.pathes.contains(0)) p->drawPath(Painter.pathes[0]);
-  popPainters  ( p )         ;
-}
-
-void N::VcfRuler::CreatePath(void)
-{
-  Painter.pathes[0] = QPainterPath() ;
-  switch (direction)                 {
-    case North                       :
-      PaintNorth(&Painter.pathes[0]) ;
-    break                            ;
-    case South                       :
-      PaintSouth(&Painter.pathes[0]) ;
-    break                            ;
-    case East                        :
-      PaintEast (&Painter.pathes[0]) ;
-    break                            ;
-    case West                        :
-      PaintWest (&Painter.pathes[0]) ;
-    break                            ;
-  }                                  ;
-}
-
-void N::VcfRuler::PaintNorth(QPainterPath * p)
-{
-  QRectF  dr = ScreenRect ;
-  qreal   ep = dr.width ();
-  qreal   dh = dr.height();
-  qreal   mh = dh / 2     ;
-  qreal   mm = dh / 4     ;
-  qreal   dl = 0          ;
-  int     di = 0          ;
-  QPointF xy              ;
-  do {
-    dl  = di ;
-    dl /= 10 ;
-    xy.setX(dl);
-    xy.setY(dl);
-    xy  = toPaper(xy);
-    dl  = xy.x();
-    if (dl<ep) {
-      p->moveTo(dr.left()+dl,dr.bottom());
-      if ((di%10)==0) p->lineTo(dr.left()+dl,dr.top   ()   ); else
-      if ((di% 5)==0) p->lineTo(dr.left()+dl,dr.bottom()-mh); else
-                      p->lineTo(dr.left()+dl,dr.bottom()-mm);
-    };
-    di ++    ;
-  } while (dl<ep);
-}
-
-void N::VcfRuler::PaintSouth(QPainterPath * p)
-{
-  QRectF  dr = ScreenRect  ;
-  qreal   ep = dr.width () ;
-  qreal   dh = dr.height() ;
-  qreal   mh = dh / 2      ;
-  qreal   mm = dh / 4      ;
-  qreal   dl = 0           ;
-  int     di = 0           ;
-  QPointF xy               ;
-  do {
-    dl  = di ;
-    dl /= 10 ;
-    xy.setX(dl);
-    xy.setY(dl);
-    xy  = toPaper(xy);
-    dl  = xy.x();
-    if (dl<ep) {
-      p->moveTo(dr.left()+dl,dr.top());
-      if ((di%10)==0) p->lineTo(dr.left()+dl,dr.bottom()   ); else
-      if ((di% 5)==0) p->lineTo(dr.left()+dl,dr.top   ()+mh); else
-                      p->lineTo(dr.left()+dl,dr.top   ()+mm);
-    };
-    di ++ ;
-  } while (dl<ep);
-}
-
-void N::VcfRuler::PaintEast(QPainterPath * p)
-{
-  QRectF  dr = ScreenRect  ;
-  qreal   ep = dr.height() ;
-  qreal   dw = dr.width () ;
-  qreal   mw = dw / 2      ;
-  qreal   mm = dw / 4      ;
-  qreal   dl = 0           ;
-  int     di = 0           ;
-  QPointF xy               ;
-  do {
-    dl  = di ;
-    dl /= 10 ;
-    xy.setX(dl);
-    xy.setY(dl);
-    xy  = toPaper(xy);
-    dl  = xy.x();
-    if (dl<ep) {
-      p->moveTo(dr.left(),dr.top()+dl);
-      if ((di%10)==0) p->lineTo(dr.right()   ,dr.top()+dl); else
-      if ((di% 5)==0) p->lineTo(dr.left ()+mw,dr.top()+dl); else
-                      p->lineTo(dr.left ()+mm,dr.top()+dl);
-    };
-    di ++ ;
-  } while (dl<ep);
-}
-
-void N::VcfRuler::PaintWest(QPainterPath * p)
-{
-  QRectF  dr = ScreenRect  ;
-  qreal   ep = dr.height() ;
-  qreal   dw = dr.width () ;
-  qreal   mw = dw / 2      ;
-  qreal   mm = dw / 4      ;
-  qreal   dl = 0           ;
-  int     di = 0           ;
-  QPointF xy               ;
-  do {
-    dl  = di ;
-    dl /= 10 ;
-    xy.setX(dl);
-    xy.setY(dl);
-    xy  = toPaper(xy);
-    dl  = xy.x();
-    if (dl<ep) {
-      p->moveTo(dr.right(),dr.top()+dl);
-      if ((di%10)==0) p->lineTo(dr.left ()   ,dr.top()+dl); else
-      if ((di% 5)==0) p->lineTo(dr.right()-mw,dr.top()+dl); else
-                      p->lineTo(dr.right()-mm,dr.top()+dl);
-    };
-    di ++ ;
-  } while (dl<ep);
-}
-"""
