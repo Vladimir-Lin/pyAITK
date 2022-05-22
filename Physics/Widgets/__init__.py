f638fbadf230c8798a302e29d11659516da1e16d
foxman <lin.vladimir@gmail.com>
2022-05-22 14:42:51 +0800

    Commit AITK for Python Library

--- Physics/Widgets/__init__.py
+++ Physics/Widgets/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 from . VtkPlanet import VtkPlanet as VtkPlanet
+from . VtkSolar  import VtkSolar  as VtkSolar
 from . VtkAtlas  import VtkAtlas  as VtkAtlas
 ##############################################################################
 __all__ = [ "VtkPlanet"                                                    , \
+            "VtkSolar"                                                     , \
             "VtkAtlas"                                                       ]
 ##############################################################################
