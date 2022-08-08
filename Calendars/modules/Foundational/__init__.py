ae3d325c9e979410f6a30814826599d501390e86
Vladimir Lin <lin.vladimir@gmail.com>
2022-08-09 03:07:33 +0800

    Commit AITK for Python Library

--- Calendars/modules/Foundational/__init__.py
+++ Calendars/modules/Foundational/__init__.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 from . GeologicTimeScale import GeologicTimeScale as GeologicTimeScale
-from . JulianDay         import JulianDay         as JulianDay
-from . Light             import Light             as Light
-from . LilianDay         import LilianDay         as LilianDay
-from . TAUD              import TAUD              as TAUD
 ##############################################################################
-__all__ = [ "GeologicTimeScale"                                            , \
-            "JulianDay"                                                    , \
-            "Light"                                                        , \
-            "LilianDay"                                                    , \
-            "TAUD"                                                           ]
+__all__ = [ "GeologicTimeScale"                                              ]
 ##############################################################################
