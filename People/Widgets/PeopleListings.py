87bf362c93bba39aad6244961ef1032dfb9cd38d
Vladimir Lin <lin.vladimir@gmail.com>
2023-08-04 20:32:24 +0800

    Commit AITK for Python Library

--- People/Widgets/PeopleListings.py
+++ People/Widgets/PeopleListings.py
@@ -48,8 +48,8 @@ from   AITK  . Qt . SpinBox           import SpinBox     as SpinBox
 ##############################################################################
 from   AITK  . Essentials . Relation  import Relation
 ##############################################################################
-from   AITK . Calendars . StarDate    import StarDate
-from   AITK . Calendars . Periode     import Periode
+from   AITK  . Calendars  . StarDate  import StarDate
+from   AITK  . Calendars  . Periode   import Periode
 ##############################################################################
 class PeopleListings               ( TreeDock                              ) :
   ############################################################################
