773f873c78ef217c4fe6a84fe275afb1a6014d6b
Vladimir Lin <lin.vladimir@gmail.com>
2023-08-11 16:44:25 +0800

    Commit AITK for Python Library

--- Documents/Widgets/__init__.py
+++ Documents/Widgets/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-from . FileExtListings       import FileExtListings       as FileExtListings
-from . MimeExtListings       import MimeExtListings       as MimeExtListings
-from . MimeTypeListings      import MimeTypeListings      as MimeTypeListings
+from . FileExtListings  import FileExtListings  as FileExtListings
+from . MimeExtListings  import MimeExtListings  as MimeExtListings
+from . MimeTypeListings import MimeTypeListings as MimeTypeListings
+from . SentenceListings import SentenceListings as SentenceListings
 ##############################################################################
 __all__ = [ "FileExtListings"                                              , \
             "MimeExtListings"                                              , \
-            "MimeTypeListings"                                               ]
+            "MimeTypeListings"                                             , \
+            "SentenceListings"                                               ]
 ##############################################################################
