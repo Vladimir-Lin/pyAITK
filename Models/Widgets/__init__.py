eb2be07124e99fcfd07a34d62480d1fe0b55b5b0
Vladimir Lin <lin.vladimir@gmail.com>
2023-09-05 21:08:29 +0800

    Commit AITK for Python Library

--- Models/Widgets/__init__.py
+++ Models/Widgets/__init__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-## Lastest : 2023-06-07
+## Lastest : 2023-09-05
 ## 模型視窗元件
 ##############################################################################
 from . ModelListings import ModelListings as ModelListings
 from . LodListings   import LodListings   as LodListings
 from . VtkModel      import VtkModel      as VtkModel
+from . VtkModelPad   import VtkModelPad   as VtkModelPad
 ##############################################################################
 __all__ = [ "ModelListings"                                                  ,
             "LodListings"                                                    ,
-            "VtkModel"                                                       ]
+            "VtkModel"                                                       ,
+            "VtkModelPad"                                                    ]
 ##############################################################################
