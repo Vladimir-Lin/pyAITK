935ca441f71c85c194c37798e9726374ad07316f
Vladimir Lin <lin.vladimir@gmail.com>
2024-06-15 09:38:58 +0800

    Commit AITK for Python Library at 2024-06-15-09-38-58

--- Videos/Player/__init__.py
+++ Videos/Player/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 from . Player   import Player   as Player
+from . Panel    import Panel    as Panel
 from . PlayList import PlayList as PlayList
 ##############################################################################
 __all__ = [ "Player"                                                       , \
+            "Panel"                                                        , \
             "PlayList"                                                       ]
 ##############################################################################
