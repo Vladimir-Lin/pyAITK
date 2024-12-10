9622cb5dbb90e69b057552e3d71046b6ac7141f4
Vladimir Lin <lin.vladimir@gmail.com>
2024-12-10 20:08:44 +0800

    Commit AITK for Python Library at 2024-12-10-20-08-41

--- Society/Widgets6/OccupationListings.py
+++ Society/Widgets6/OccupationListings.py
@@ -11,59 +11,38 @@ import requests
 import threading
 import json
 ##############################################################################
-from   PyQt5                          import QtCore
-from   PyQt5                          import QtGui
-from   PyQt5                          import QtWidgets
+from   PySide6                         import QtCore
+from   PySide6                         import QtGui
+from   PySide6                         import QtWidgets
+from   PySide6 . QtCore                import *
+from   PySide6 . QtGui                 import *
+from   PySide6 . QtWidgets             import *
+from   AITK    . Qt6                   import *
 ##############################################################################
-from   PyQt5 . QtCore                 import QObject
-from   PyQt5 . QtCore                 import pyqtSignal
-from   PyQt5 . QtCore                 import pyqtSlot
-from   PyQt5 . QtCore                 import Qt
-from   PyQt5 . QtCore                 import QPoint
-from   PyQt5 . QtCore                 import QPointF
-from   PyQt5 . QtCore                 import QSize
+from   AITK    . Qt6 . MenuManager     import MenuManager as MenuManager
+from   AITK    . Qt6 . TreeDock        import TreeDock    as TreeDock
+from   AITK    . Qt6 . LineEdit        import LineEdit    as LineEdit
+from   AITK    . Qt6 . ComboBox        import ComboBox    as ComboBox
+from   AITK    . Qt6 . SpinBox         import SpinBox     as SpinBox
 ##############################################################################
-from   PyQt5 . QtGui                  import QIcon
-from   PyQt5 . QtGui                  import QCursor
-from   PyQt5 . QtGui                  import QKeySequence
+from   AITK    . Essentials . Relation import Relation
+from   AITK    . Calendars  . StarDate import StarDate
+from   AITK    . Calendars  . Periode  import Periode
+from   AITK    . People     . People   import People
 ##############################################################################
-from   PyQt5 . QtWidgets              import QApplication
-from   PyQt5 . QtWidgets              import QWidget
-from   PyQt5 . QtWidgets              import qApp
-from   PyQt5 . QtWidgets              import QAction
-from   PyQt5 . QtWidgets              import QShortcut
-from   PyQt5 . QtWidgets              import QMenu
-from   PyQt5 . QtWidgets              import QAbstractItemView
-from   PyQt5 . QtWidgets              import QTreeWidget
-from   PyQt5 . QtWidgets              import QTreeWidgetItem
-from   PyQt5 . QtWidgets              import QLineEdit
-from   PyQt5 . QtWidgets              import QComboBox
-from   PyQt5 . QtWidgets              import QSpinBox
-##############################################################################
-from   AITK  . Qt . MenuManager       import MenuManager as MenuManager
-from   AITK  . Qt . TreeDock          import TreeDock    as TreeDock
-from   AITK  . Qt . LineEdit          import LineEdit    as LineEdit
-from   AITK  . Qt . ComboBox          import ComboBox    as ComboBox
-from   AITK  . Qt . SpinBox           import SpinBox     as SpinBox
-##############################################################################
-from   AITK  . Essentials . Relation  import Relation
-from   AITK  . Calendars  . StarDate  import StarDate
-from   AITK  . Calendars  . Periode   import Periode
-from   AITK  . People     . People    import People
-##############################################################################
-class OccupationListings           ( TreeDock                              ) :
+class OccupationListings     ( TreeDock                                    ) :
   ############################################################################
   HavingMenu        = 1371434312
   ############################################################################
-  emitNamesShow     = pyqtSignal   (                                         )
-  emitAllNames      = pyqtSignal   ( dict                                    )
-  emitAssignAmounts = pyqtSignal   ( str , int                               )
-  PeopleGroup       = pyqtSignal   ( str , int , str                         )
-  OpenLogHistory    = pyqtSignal   ( str , str , str , str , str           )
+  emitNamesShow     = Signal (                                               )
+  emitAllNames      = Signal ( dict                                          )
+  emitAssignAmounts = Signal ( str , int                                     )
+  PeopleGroup       = Signal ( str , int , str                               )
+  OpenLogHistory    = Signal ( str , str , str , str , str                   )
   ############################################################################
-  def __init__                     ( self , parent = None , plan = None    ) :
+  def __init__               ( self , parent = None , plan = None          ) :
     ##########################################################################
-    super ( ) . __init__           (        parent        , plan             )
+    super ( ) . __init__     (        parent        , plan                   )
     ##########################################################################
     self . EditAllNames       = None
     ##########################################################################
@@ -237,21 +216,18 @@ class OccupationListings           ( TreeDock                              ) :
     ##########################################################################
     return IT
   ############################################################################
-  @pyqtSlot                  (                                               )
   def InsertItem             ( self                                        ) :
     ##########################################################################
     self . defaultInsertItem ( 0 , "editingFinished" , self . nameChanged    )
     ##########################################################################
     return
   ############################################################################
-  @pyqtSlot             (                                                    )
   def RenameItem        ( self                                             ) :
     ##########################################################################
     self . goRenameItem ( 0                                                  )
     ##########################################################################
     return
   ############################################################################
-  @pyqtSlot                      (                                           )
   def nameChanged                ( self                                    ) :
     ##########################################################################
     if                           ( not self . isItemPicked ( )             ) :
@@ -276,7 +252,6 @@ class OccupationListings           ( TreeDock                              ) :
     ##########################################################################
     return
   ############################################################################
-  @pyqtSlot                        (        dict                             )
   def refresh                      ( self , JSON                           ) :
     ##########################################################################
     self    . clear                (                                         )
@@ -314,7 +289,6 @@ class OccupationListings           ( TreeDock                              ) :
     ##########################################################################
     return NAMEs
   ############################################################################
-  @pyqtSlot                (        str  , int                               )
   def AssignAmounts        ( self , UUID , Amounts                         ) :
     ##########################################################################
     IT = self . uuidAtItem ( UUID , 0                                        )
@@ -401,7 +375,6 @@ class OccupationListings           ( TreeDock                              ) :
     ##########################################################################
     return
   ############################################################################
-  @pyqtSlot          (                                                       )
   def startup        ( self                                                ) :
     ##########################################################################
     if               ( not self . isPrepared ( )                           ) :
