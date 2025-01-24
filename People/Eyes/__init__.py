a90acc880a2a122c6e147e175cdf810ac4b91b0d
Vladimir Lin <lin.vladimir@gmail.com>
2025-01-24 19:25:41 +0800

    Commit AITK for Python Library at 2025-01-24-19-25-40

--- People/Eyes/__init__.py
+++ People/Eyes/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-from . Eyes import Eyes as Eyes
-from . Iris import Iris as Iris
+from . Eyes      import Eyes      as Eyes
+from . EyeShapes import EyeShapes as EyeShapes
+from . Iris      import Iris      as Iris
 ##############################################################################
 __all__ = [ "Eyes"                                                         , \
+            "EyeShapes"                                                    , \
             "Iris"                                                           ]
 ##############################################################################
--- People/Faces/__init__.py
+++ People/Faces/__init__.py
@@ -4,11 +4,13 @@ from . VcfFaceRegion import VcfFaceRegion as VcfFaceRegion
 from . VtkRawFace    import VtkRawFace    as VtkRawFace
 from . VtkFace       import VtkFace       as VtkFace
 from . Face          import Face          as Face
+from . FaceShapes    import FaceShapes    as FaceShapes
 from . FaceView      import FaceView      as FaceView
 ##############################################################################
 __all__ = [ "VcfFaceRegion"                                                , \
             "VtkRawFace"                                                   , \
             "VtkFace"                                                      , \
             "Face"                                                         , \
+            "FaceShapes"                                                   , \
             "FaceView"                                                       ]
 ##############################################################################
