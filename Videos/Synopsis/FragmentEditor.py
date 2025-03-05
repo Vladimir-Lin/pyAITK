909fad69906c4af8326560b92e37999c05ca389a
Vladimir Lin <lin.vladimir@gmail.com>
2025-03-05 19:43:11 +0800

    Commit AITK for Python Library at 2025-03-05-19-43-11

---
+++ Videos/Synopsis/FragmentEditor.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+## Chapter
+## 影片章節
+##############################################################################
+import os
+import sys
+import getopt
+import time
+import requests
+import threading
+import gettext
+import json
+import vlc
+import math
+import cv2
+##############################################################################
+import pathlib
+from   pathlib                           import Path
+##############################################################################
+import AITK
+##############################################################################
+from   AITK    . Calendars . StarDate    import StarDate    as StarDate
+from   AITK    . Documents . JSON        import Load        as LoadJson
+from   AITK    . Documents . JSON        import Save        as SaveJson
+##############################################################################
+from   PySide6                           import QtCore
+from   PySide6                           import QtGui
+from   PySide6                           import QtWidgets
+from   PySide6 . QtCore                  import *
+from   PySide6 . QtGui                   import *
+from   PySide6 . QtWidgets               import *
+from   AITK    . Qt6                     import *
+##############################################################################
+from   AITK    . Qt6 . MenuManager       import MenuManager as MenuManager
+from   AITK    . Qt6 . AttachDock        import AttachDock  as AttachDock
+from   AITK    . Qt6 . Widget            import Widget      as Widget
+##############################################################################
+from   AITK    . AI  . Pictures . Vision import Vision      as AiVision
+##############################################################################
+from   AITK    . Pictures . Picture      import Picture     as PictureItem
+from   AITK    . People . Faces . Face   import Face        as FaceItem
+from   AITK    . People . Body  . Tit    import Tit         as TitItem
+from   AITK    . People . Body  . Body   import Body        as BodyItem
+##############################################################################
