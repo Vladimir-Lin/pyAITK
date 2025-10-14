f8b8b665f23d297896090965f85ab7792ee2da6d
Vladimir Lin <lin.vladimir@gmail.com>
2025-10-15 06:17:03 +0800

    Commit AITK for Python Library at 2025-10-15-06-17-03

--- Videos/Subtitles.bat
+++ Videos/Subtitles.bat
@@ -1,9 +1,11 @@
+cd ..\videos
 ffmpeg.exe \
 -i video.mp4 \
--i metadata.txt \
--i EN.ass \
--i TW.ass \
--i CN.ass \
+-i ..\scripts\metadata.txt \
+-i ..\subtitles\EN\EN.ass \
+-i ..\subtitles\TW\TW.ass \
+-i ..\subtitles\CN\CN.ass \
+-i ..\subtitles\JP\JP.ass \
 -c copy -map_metadata 1 -map 0:v -map 0:a \
 -map 2 -c:s mov_text -metadata:s:s:0 language=eng \
 -map 3 -c:s mov_text -metadata:s:s:1 language=zht \
