01fcd1492a816c7814dc551a76ca7a70bf27762e
Vladimir Lin <lin.vladimir@gmail.com>
2026-05-30 12:18:35 +0800

    Commit AITK for Python Library at 2026-05-30-12-18-35

--- Videos/Synopsis/ScenarioEditor.py
+++ Videos/Synopsis/ScenarioEditor.py
@@ -10,6 +10,9 @@ import requests
 import threading
 import json
 ##############################################################################
+import pathlib
+from   pathlib                         import Path
+##############################################################################
 from   PySide6                         import QtCore
 from   PySide6                         import QtGui
 from   PySide6                         import QtWidgets
