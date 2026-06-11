d3d3004395d8f045eeb9a64cacc9e8f6d51d4918
Vladimir Lin <lin.vladimir@gmail.com>
2026-06-11 22:00:42 +0800

    Commit AITK for Python Library at 2026-06-11-22-00-42

--- Videos/MoveSubtitles.bat
+++ Videos/MoveSubtitles.bat
@@ -1,5 +1,5 @@
 cd ..\videos
-copy /y EN.ass ..\subtitles\EN\EN.ass
-copy /y TW.ass ..\subtitles\TW\TW.ass
-copy /y CN.ass ..\subtitles\CN\CN.ass
-copy /y JP.ass ..\subtitles\JP\JP.ass
+move /y EN.ass ..\subtitles\EN\EN.ass
+move /y TW.ass ..\subtitles\TW\TW.ass
+move /y CN.ass ..\subtitles\CN\CN.ass
+move /y JP.ass ..\subtitles\JP\JP.ass
